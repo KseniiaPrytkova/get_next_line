15009cfbb5f4ba9032e14ebb6c25df427ee18f9e
kseniiaprytkova <prytkovakseniia@gmail.com>
2018-03-15 18:44:30 +0200

    reads specific nb of bytes once

--- get_next_line.c
+++ get_next_line.c
@@ -12,22 +12,23 @@
 #include "get_next_line.h"
 
 // int		i_am_for_reading(int fd, char *buffer)
-char		i_am_for_reading(int fd)
+char		*i_am_for_reading(int fd)
 {
 	int		read_bytes;
-	char	buffer[BUFF_SIZE + 1];
+	char	*buffer;
 	
 
-	// if (!(buffer = malloc(sizeof(char) * BUFF_SIZE + 1)))
-	// 	return (0);
-	while ((read_bytes = read(fd, buffer, BUFF_SIZE)) > 0)
-	{
-		buffer[read_bytes] = '|';
-		ft_putstr(buffer);
-	}
-	ft_putchar('\n');
-
-	return(*buffer);
+	if (!(buffer = malloc(sizeof(char) * BUFF_SIZE + 1)))
+		return (NULL);
+	// while ((read_bytes = read(fd, buffer, BUFF_SIZE)) > 0)
+	// {
+	// 	buffer[read_bytes] = '|';
+	// 	ft_putstr(buffer);
+	// }
+	// ft_putchar('\n');
+	read_bytes = read(fd, buffer, BUFF_SIZE);
+	buffer[read_bytes] = '\0';
+	return(buffer);
 	
 }
 
@@ -39,9 +40,10 @@ int get_next_line(const int fd, char **line)
 	printf("BUFF_SIZE = %i\n", BUFF_SIZE );
 	if (fd == -1)
 		return (0);
-	*result_string = i_am_for_reading(fd);
+	result_string = i_am_for_reading(fd);
+	printf("%s\n", result_string );
 
-	printf("Q!!!:%s\n", result_string );
+	
 
 	return (0);
 }
