7e0142b3c438a9d46e120a10880dea615701cc50
kseniiaprytkova <prytkovakseniia@gmail.com>
2018-03-21 13:11:53 +0200

    i_am_for reading was fixed

--- get_next_line.c
+++ get_next_line.c
@@ -15,64 +15,49 @@
 char		*i_am_for_reading(int fd, char *result_string)
 {
 	int				read_bytes;
-	char			*buffer_for_read;
-	char			*buffer_for_join[BUFF_SIZE + 1];
-	static int		is_it_first_time;
+	char			buffer_for_read[BUFF_SIZE + 1];
+	// char			buffer_for_join[BUFF_SIZE + 1];
 	int 			counter;
 	size_t			res_str_len;
 	// char			*pointer_to_result_string;
 
-	is_it_first_time = 0;
 	res_str_len = 0;
-	counter = 0;
-	if (!(buffer_for_read = malloc(sizeof(char) * BUFF_SIZE + 1)))
-		return (NULL);
-
-
 	while ((read_bytes = read(fd, buffer_for_read, BUFF_SIZE)) > 0)
 	{
 		buffer_for_read[read_bytes] = '\0';
 
-		printf("I'M BUFFER: %s\n", buffer_for_read);
-		if (is_it_first_time == 0)
+		//printf("I'M BUFFER: %s\n", buffer_for_read);
+		if (!(result_string)) //edit - is_it_f_time
 		{
-			printf("---->>>I'M IN THE 1ST TIME --->>>\n");
-			ft_bzero(result_string, BUFF_SIZE + 1);
-			// ft_memccpy(result_string, buffer_for_read, '\0', BUFF_SIZE);
-			printf("catch seg_fault!\n");
-			ft_memcpy(result_string, buffer_for_read, BUFF_SIZE);
-			printf("%s\n", result_string );
-			is_it_first_time = 1;
-			printf("CATCH U!!!\n" );
-
+			//printf("---->>>I'M IN THE 1ST TIME --->>>\n");
+			if (!(result_string = malloc(sizeof(char) * read_bytes + 1)))
+				return (NULL);
+			ft_bzero(result_string, read_bytes + 1);
+			//printf("catch seg_fault!\n");
+			ft_memcpy(result_string, buffer_for_read, read_bytes);
+			//printf("%s\n", result_string );
+			//printf("CATCH U!!!\n" );
 		}
-		if (is_it_first_time >  1)
+		else
 		{
-			printf("--->>>>i'm in join!!!\n");
-			printf("ITERATION #:%i\n", counter );
-
-			ft_bzero(buffer_for_join, BUFF_SIZE + 1);
-			ft_memcpy(buffer_for_join, buffer_for_read, BUFF_SIZE);
-
-			printf("BUFFER_FOR_JOIN: %s\n", buffer_for_join );
-			printf("result_string HERE:%s\n", result_string );
-
-			result_string = ft_strjoin(result_string, buffer_for_join);
-
-			printf("AFTER_CONCATENATION: %s\n", result_string );
-			is_it_first_time = 2;
-			printf("\n" );
+			//printf("--->>>>i'm in join!!!\n");
+			//printf("ITERATION #:%i\n", counter );
+			// ft_bzero(buffer_for_join, BUFF_SIZE + 1);
+			// ft_memcpy(buffer_for_join, buffer_for_read, BUFF_SIZE);
+			//printf("BUFFER_FOR_JOIN: %s\n", buffer_for_join );
+			//printf("result_string HERE:%s\n", result_string );
+			result_string = ft_strjoin(result_string, buffer_for_read); // CHANGE STR JOIN TO FREE STR JOIN!!!!
+			//printf("AFTER_CONCATENATION: %s\n", result_string );
+			//printf("\n" );
 		}
-
-		is_it_first_time ++;
 		
 	}
-	printf("len of result str: %zu\n", ft_strlen (result_string));
+	//printf("len of result str: %zu\n", ft_strlen (result_string));
 
 	res_str_len = ft_strlen (result_string);
 	result_string[res_str_len] = '\0';
 
-	printf("%c\n", result_string[res_str_len - 1] );
+	//printf("%c\n", result_string[res_str_len - 1] );
 
 	return(result_string);
 	
@@ -80,19 +65,19 @@ char		*i_am_for_reading(int fd, char *result_string)
 
 int get_next_line(const int fd, char **line)
 {
-	char	*result_string;
+	static char	*result_string; //edit
 
-	printf("The value of FD: %i\n", fd );
-	printf("BUFF_SIZE = %i\n", BUFF_SIZE );
+	//printf("The value of FD: %i\n", fd );
+	//printf("BUFF_SIZE = %i\n", BUFF_SIZE );
 	if (fd == -1)
 		return (0);
 
 	result_string = i_am_for_reading(fd, result_string);
-	printf("\n");
+	//printf("\n");
 
-	printf("I AM HERE --->>>\n");
+	//printf("I AM HERE --->>>\n");
 	printf("%s\n", result_string );
-
+	while (1){};
 	
 
 	return (0);
